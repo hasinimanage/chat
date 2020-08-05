612dd45d4d345154ca5fd37bcb59a95550499fca
unknown <hasinimanage@gmail.com>
2020-08-05 05:54:17 +0530

    Login Screen

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import{SafeAreaView,Text,View} from 'react-native';
 import {globalStyle,color} from '../../utility';
-import {Logo} from '../../component';
+import {Logo,InputField,RoundCornerButton} from '../../component';
 
 
 const Login=({navigation})=>{
@@ -10,6 +10,24 @@ const Login=({navigation})=>{
         
             <View style={[globalStyle.containerCentered]}>
               <Logo />
+            </View>
+
+            <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
+                 <InputField placeholder="Enter email"/>
+                 <InputField placeholder="Enter password" secureTextEntry={true}/>
+                 <RoundCornerButton title="Login" />
+                 <Text
+                  style={{
+                      fontSize:20,
+                      fontWeight:'bold',
+                      color:color.LIGHT_GREEN,
+                  }}>
+                   Sign Up
+                  </Text>
+
+
+
+              
             </View>
         
         </SafeAreaView>
