570c9584049d116d288dc9f26bd6c2f44630427e
unknown <hasinimanage@gmail.com>
2020-08-05 06:48:26 +0530

    Alret functions with credentials

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import{SafeAreaView,Text,View} from 'react-native';
 import {globalStyle,color} from '../../utility';
 import {Logo,InputField,RoundCornerButton} from '../../component';
 
 
 const Login=({navigation})=>{
+const [credentials,setCredentials] =useState({
+    email:"",
+    password:"",
+});
+
+const {email,password} = credentials;
+
+
+const onLoginPress = () => {
+    alert(JSON.stringify(credentials));
+
+};
+
+const handleOnChange = (name, value) => {
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
     return(
         <SafeAreaView style={[globalStyle.flex1, {backgroundColor:color.BLACK}]}>
         
@@ -13,15 +32,26 @@ const Login=({navigation})=>{
             </View>
 
             <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
-                 <InputField placeholder="Enter email"/>
-                 <InputField placeholder="Enter password" secureTextEntry={true}/>
-                 <RoundCornerButton title="Login" />
+                 <InputField placeholder="Enter email" value={email}
+                 onChangeText={(text)=>handleOnChange('email',text)}
+
+                 />
+                 <InputField placeholder="Enter password" secureTextEntry={true}
+                 value={password}
+                 onChangeText={(text)=>handleOnChange('password',text)}
+
+                 />
+                 <RoundCornerButton title="Login" onPress={() => onLoginPress()} />
                  <Text
                   style={{
-                      fontSize:20,
+                      fontSize:28,
                       fontWeight:'bold',
                       color:color.LIGHT_GREEN,
-                  }}>
+                  
+                    }}
+                    onPress={() => navigation.navigate('SignUp')}>
+
+
                    Sign Up
                   </Text>
 
