80e07d7a66906878c8633e5317b35b8c3214f1db
unknown <hasinimanage@gmail.com>
2020-08-05 08:54:44 +0530

    Sign Up screen

--- src/container/SignUp/index.js
+++ src/container/SignUp/index.js
@@ -1,13 +1,91 @@
-import React from 'react';
-import{View,Text} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { color } from "../../utility";
+import React, { useState } from 'react';
+import{SafeAreaView,Text,View} from 'react-native';
+import {globalStyle,color} from '../../utility';
+import {Logo,InputField,RoundCornerButton} from '../../component';
 
 
 const SignUp=({navigation})=>{
+const [credentials,setCredentials] =useState({
+    name:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+});
+
+const {name,email,password,confirmPassword} = credentials;
+
+
+const onSignUpPress = () => {
+    if(!name){
+        alert('Name is required');
+      }
+    else if(!email){
+      alert('Email is required');
+    }else if(!password){
+        alert('Password is required');
+
+    }else if(password !== confirmPassword){
+        alert('Password did not match');
+      }
+    else{
+        alert(JSON.stringify(credentials));
+    }
+    
+
+};
+
+const handleOnChange = (name, value) => {
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
     return(
-        <SafeAreaView>
-            <Text onPress={() => navigation.navigate('Dashboard')}>SignUp</Text>
+        <SafeAreaView style={[globalStyle.flex1, {backgroundColor:color.BLACK}]}>
+        
+            <View style={[globalStyle.containerCentered]}>
+              <Logo />
+            </View>
+
+            <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
+               <InputField placeholder="Enter name" value={name}
+                 onChangeText={(text)=>handleOnChange('name',text)}
+
+                 />
+                 <InputField placeholder="Enter email" value={email}
+                 onChangeText={(text)=>handleOnChange('email',text)}
+
+                 />
+                 <InputField placeholder="Enter password" secureTextEntry={true}
+                 value={password}
+                 onChangeText={(text)=>handleOnChange('password',text)}
+
+                 />
+                 <InputField placeholder="Confirm password" secureTextEntry={true}
+                 value={confirmPassword}
+                 onChangeText={(text)=>handleOnChange('confirmPassword',text)}
+
+                 />
+                 
+                 <RoundCornerButton title="SignUp" onPress={() => onSignUpPress()} />
+                 <Text
+                  style={{
+                      fontSize:28,
+                      fontWeight:'bold',
+                      color:color.LIGHT_GREEN,
+                  
+                    }}
+                    onPress={() => navigation.navigate('Login')}>
+
+
+                   Login
+                  </Text>
+
+
+
+              
+            </View>
+        
         </SafeAreaView>
     );
 };
