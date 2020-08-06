87c2f5331325be195e47c1f0e90ac5802e93c98d
unknown <hasinimanage@gmail.com>
2020-08-07 02:37:20 +0530

    Adding camera and send icons

--- src/container/Chat/index.js
+++ src/container/Chat/index.js
@@ -1,6 +1,9 @@
 import React, {useLayoutEffect} from 'react';
 import {View,Text,SafeAreaView,FlatList} from 'react-native';
-import { color, globalStyle } from '../../utility';
+import { color, globalStyle,appStyle } from '../../utility';
+import styles from './styles';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import{InputField} from '../../component'
 
 const Chat = ({route,navigation}) => {
     const{params}=route;
@@ -14,12 +17,37 @@ const Chat = ({route,navigation}) => {
     return(
         <SafeAreaView style={[globalStyle.flex1,{backgroundColor:color.BLACK}]}> 
          <FlatList
+           inverted
            data={[1,2,3]}
            keyExtractor ={(_, index)=> index.toString()}
            renderItem={(item)=>(
                <Text style={{color:color.WHITE}}>{name}</Text>
            )}
          />
+
+         {/* Send Message*/}
+          <View style={styles.sendMessageContainer}>
+            <InputField
+              placeholder="Type Here"
+              numberOfLines={10}
+              inputStyle={styles.input}
+            />
+            <View style={styles.sendBtnContainer}>
+             <MaterialCommunityIcons
+               name="camera"
+               color={color.WHITE}
+               size={40}
+
+             />
+             <MaterialCommunityIcons
+               name="send-circle"
+               color={color.WHITE}
+               size={40}
+
+             />
+            </View>
+
+          </View>
         </SafeAreaView>
     );
 };
