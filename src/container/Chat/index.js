42d08b7986ae3de8302e59bdfcc1eb5087b40066
unknown <hasinimanage@gmail.com>
2020-08-06 23:19:10 +0530

    Texting chat Screen with dummy dat

--- src/container/Chat/index.js
+++ src/container/Chat/index.js
@@ -1,10 +1,25 @@
 import React, {useLayoutEffect} from 'react';
-import {View,Text,SafeAreaView} from 'react-native';
+import {View,Text,SafeAreaView,FlatList} from 'react-native';
+import { color, globalStyle } from '../../utility';
 
 const Chat = ({route,navigation}) => {
+    const{params}=route;
+    const {name,img,imgText,guessUserId,currentUserId}=params;
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerTitle:<Text>{name}</Text>,
+            
+        });
+    },[navigation]);
     return(
-        <SafeAreaView>
-         <Text>Chat</Text>
+        <SafeAreaView style={[globalStyle.flex1,{backgroundColor:color.BLACK}]}> 
+         <FlatList
+           data={[1,2,3]}
+           keyExtractor ={(_, index)=> index.toString()}
+           renderItem={(item)=>(
+               <Text style={{color:color.WHITE}}>{name}</Text>
+           )}
+         />
         </SafeAreaView>
     );
 };
